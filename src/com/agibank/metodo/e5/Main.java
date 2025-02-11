b23347e4465be61edf0754fbd209ef913fa0c16f
anderson.lima <andersonmlima2@gmail.com>
2025-02-11 11:51:43 -0300

    exec 5 and 6.

--- src/com/agibank/metodo/e5/Main.java
+++ src/com/agibank/metodo/e5/Main.java
@@ -2,5 +2,16 @@ package com.agibank.metodo.e5;
 
 public class Main {
     public static void main(String[] args) {
+        double ativo1 = 5.0;
+        double ativo2 = 4.5;
+        String maiorAtivo = comprarAtivos(5.0, 4.5);
+        System.out.println(maiorAtivo);
+    }
+    public static String comprarAtivos(double ativo1, double ativo2){
+        if (ativo1 > ativo2){
+            return "O maior ativo é: ";
+        } else {
+            return "O maior ativo é: ";
+        }
     }
 }
---
+++ src/com/agibank/metodo/e6/Main.java
@@ -0,0 +1,13 @@
+package com.agibank.metodo.e6;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] retornos = {0.01, 0.02, -0.1, 0.03, -0.02};
+    }
+    public static double calcularVolatilidade(double[] retornos){
+        for (int i = 0; i > retornos.length; i++){
+            double soma = 0;
+
+        }
+    }
+}
