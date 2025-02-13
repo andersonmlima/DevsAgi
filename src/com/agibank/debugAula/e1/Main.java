d5bf5a8c64979103c9df09b439a75f556748cc10
anderson.lima <andersonmlima2@gmail.com>
2025-02-13 11:54:01 -0300

    Exec debug 1 - 8

---
+++ src/com/agibank/debugAula/e1/Main.java
@@ -0,0 +1,14 @@
+package com.agibank.debugAula.e1;
+
+//calculo do retorno diário de uma ação: Dado um vetor com os preços de uma aão nos ultimos 5 dias, calcule o percentual da variação diária em relação ao dia anterior.
+// formula: ((Pi - Pi-1)/Pi-1) x 100
+//Entrada (preço diário da ação)
+// 100.0 102.0 101.0 103.0 105.0
+//Saida (variação diária)
+// 2.00 -0.98 1.98 1.94
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
