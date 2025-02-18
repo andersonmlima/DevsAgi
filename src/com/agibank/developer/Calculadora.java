37de6445b3a24aa2d31af3a493e9e73ce960aac8
anderson.lima <andersonmlima2@gmail.com>
2025-02-18 09:42:30 -0300

    Adicionado a subtração.

--- src/com/agibank/developer/Calculadora.java
+++ src/com/agibank/developer/Calculadora.java
@@ -3,6 +3,7 @@ package com.agibank.developer;
 public class Calculadora {
     public static void main(String[] args) {
         System.out.println("A soma dos numeros é: " + soma(20, 20));
+        System.out.println("A subtração dos números é: " + subtracao(15, 5));
     }
     public static int soma(int a, int b){
         return a + b;
