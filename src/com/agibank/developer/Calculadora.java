224fa38db930703d483c69bdf1922caa1c99a399
anderson.lima <andersonmlima2@gmail.com>
2025-02-18 09:41:10 -0300

    Calculadora Soma

---
+++ src/com/agibank/developer/Calculadora.java
@@ -0,0 +1,13 @@
+package com.agibank.developer;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("A soma dos numeros é: " + soma(20, 20));
+    }
+    public static int soma(int a, int b){
+        return a + b;
+    }
+    public static int subtracao(int c, int d){
+        return c - d;
+    }
+}
